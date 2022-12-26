3429cef1ca669b0542d9587ab7015d015064f0e4
Loromirov <loromirov@yandex.ru>
2022-12-26 21:26:21 +0300

    пятая задача

--- Program.cs
+++ Program.cs
@@ -8,37 +8,71 @@
 26(1,0,1) 55(1,1,1)
 */
 
-void PrintIndex(int[,,] mtrx)
+int temp = 1;
+int i = 0;
+int j = 0;
+
+
+void PrintMatrix(int[,] mtrx)
 {
     for (int i = 0; i < mtrx.GetLength(0); i++)
     {
         for (int j = 0; j < mtrx.GetLength(1); j++)
         {
-            System.Console.WriteLine();
-            for (int g = 0; g < mtrx.GetLength(2); g++)
+            if (mtrx[i, j] / 10 <= 0 )
             {
-                System.Console.Write($"{mtrx[i, j, g]}({i},{j},{g})");
+                Console.Write($" {mtrx[i, j]} ");
             }
+            else
+            {
+                Console.Write($"{mtrx[i, j]} ");
+            } 
+            
         }
+        Console.WriteLine();
     }
+    Console.WriteLine();
 }
-int[,,] GenerateRandomMatrix(int min, int max, int m, int n, int k)
+
+
+int[,] GenerateRandomMatrix(int min, int max, int m, int n)
 {
-    int[,,] mtrx = new int[m, n, k];
+    int[,] mtrx = new int[m, n];
     Random r = new Random();
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            for (int g = 0; g < k; g++)
-            {
-                mtrx[i, j, g] = r.Next(min, max + 1);
-            }
+            mtrx[i, j] = r.Next(min, max + 1);
         }
     }
     return mtrx;
 }
 
 
-int[,,] randMatrix = GenerateRandomMatrix(0, 9, 2, 2, 2);
-PrintIndex(randMatrix);
+int[,] matrix = GenerateRandomMatrix(0, 20, 4, 4);
+PrintMatrix(matrix);
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+    matrix[i,j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    {
+        j++;
+    }
+    else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    {
+        i++;
+    }
+    else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    {
+        j--;
+    }
+    else
+    {
+        i--;
+    }
+}
+
+PrintMatrix(matrix);
